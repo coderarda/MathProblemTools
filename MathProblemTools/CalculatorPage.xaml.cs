5349e860da68b27c3b5523c44bc1e9ead4a7a6b1
Arda Akcagöz <a.akcgz002@hotmail.com>
2022-12-16 19:01:21 +0300

    calculator finished

--- MathProblemTools/CalculatorPage.xaml.cs
+++ MathProblemTools/CalculatorPage.xaml.cs
@@ -4,23 +4,18 @@ using Microsoft.UI.Xaml.Controls;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace MathProblemTools
-{
+namespace MathProblemTools {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class CalculatorPage : Page
-    {
+    public sealed partial class CalculatorPage : Page {
         private bool calculateClicked = false;
-        public CalculatorPage()
-        {
-            this.InitializeComponent();
+        public CalculatorPage() {
+            InitializeComponent();
         }
 
-        private void Write(object sender, RoutedEventArgs e)
-        {
-            if (calculateClicked)
-            {
+        private void Write(object sender, RoutedEventArgs e) {
+            if (calculateClicked) {
                 Numbers.Text = "";
             }
             calculateClicked = false;
@@ -32,35 +27,25 @@ namespace MathProblemTools
                 || (string)b.Content == "-"
                 || (string)b.Content == "÷"
                 || (string)b.Content == "+"
-            )
-            {
+            ) {
                 Numbers.Text += " " + b.Content + " ";
-            }
-            else
+            } else
                 Numbers.Text += b.Content;
         }
 
-        private void Calculate(object sender, RoutedEventArgs e)
-        {
+        private void Calculate(object sender, RoutedEventArgs e) {
             calculateClicked = true;
 
-            if (Numbers.Text.Contains('x'))
-            {
+            if (Numbers.Text.Contains('x')) {
                 string[] s = Numbers.Text.Replace(" ", "").Split("x");
                 Numbers.Text = (int.Parse(s[0]) * int.Parse(s[1])).ToString();
-            }
-            else if (Numbers.Text.Contains('+'))
-            {
+            } else if (Numbers.Text.Contains('+')) {
                 string[] s = Numbers.Text.Replace(" ", "").Split("+");
                 Numbers.Text = (int.Parse(s[0]) + int.Parse(s[1])).ToString();
-            }
-            else if (Numbers.Text.Contains('÷'))
-            {
+            } else if (Numbers.Text.Contains('÷')) {
                 string[] s = Numbers.Text.Replace(" ", "").Split("÷");
                 Numbers.Text = (int.Parse(s[0]) / int.Parse(s[1])).ToString();
-            }
-            else if (Numbers.Text.Contains('-'))
-            {
+            } else if (Numbers.Text.Contains('-')) {
                 string[] s = Numbers.Text.Replace(" ", "").Split("-");
                 Numbers.Text = (int.Parse(s[0]) - int.Parse(s[1])).ToString();
             }
@@ -68,8 +53,7 @@ namespace MathProblemTools
 
         private void Page_Loaded(object sender, RoutedEventArgs e) { }
 
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
+        private void ClearButton_Click(object sender, RoutedEventArgs e) {
             Numbers.Text = "";
         }
     }
--- MathProblemTools/MainWindow.xaml.cs
+++ MathProblemTools/MainWindow.xaml.cs
@@ -1,42 +1,33 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace MathProblemTools
-{
+namespace MathProblemTools {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public sealed partial class MainWindow : Window {
+        private string selectedItemName;
+        public MainWindow() {
             InitializeComponent();
             MainFrame.Navigate(typeof(CalculatorPage));
         }
 
-        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            switch(args.InvokedItemContainer.Content)
-            {
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args) {
+            switch (args.InvokedItemContainer.Content) {
                 case "Home":
-                    MainFrame.Navigate(typeof(CalculatorPage));
+                    if (args.InvokedItemContainer.Content.ToString() != selectedItemName) {
+                        MainFrame.Navigate(typeof(CalculatorPage));
+                    }
                     break;
             }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
+            selectedItemName = args.SelectedItemContainer.Content.ToString();
         }
     }
 }
